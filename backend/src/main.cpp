05e1bd570856b3798950883e1bbecfbf61898f5c
ndrandal <nickrandall317@gmail.com>
2025-06-01 16:53:26 -0400

    init

---
+++ backend/src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Chart backend server starting..." << std::endl;
+    // TODO: Initialize WebSocket server in a future step
+    return 0;
+}
---
+++ frontend/shaders/vertex.glsl
@@ -0,0 +1,7 @@
+
+#version 300 es
+layout(location = 0) in vec2 a_position;
+uniform mat4 u_projection;
+void main() {
+    gl_Position = u_projection * vec4(a_position, 0.0, 1.0);
+}
